cbfee0b871cc7f73b9fe337113b036780ef74858
anukranan <krana@krana-pc.localdomain>
2022-06-11 15:10:16 -0400

    Better structure and compilation.

--- Makefile
+++ Makefile
@@ -1,40 +1,55 @@
-#=================
-# Makefile for radical-reducer
-#=================
+#============
+# Makefile for simplify_radicals.
+#============
 
-#===========================
+.POSIX:
 
-CCOMPILER   ?= gcc
-CFLAGS       = -std=c99 -Wall -Wextra -static
-LFLAGS       = -lm
+#========================
 
-BUILD        = build
-BIN          = bin
-TARGET       = radical-reducer
-PREFIX      ?= /usr/local
+# Compiler
+CC       = cc
 
-SRC          = main.c reduce.c
-OBJ          = $(SRC:.c=.o)
+# Flags
+INCLUDE  =
+CFLAGS   = $(INCLUDE) -std=c99 -Wall -Wno-unused -O3 -static
+LFLAGS   = -lm
 
-#============================
+# Folders
+BINDIR   = bin
+BUILDDIR = build
+SRCDIR   = src
+
+# Installation
+TARGET   = radical-reducer
+PREFIX  ?= /usr/local/bin
+
+# Compilation
+SRC      = $(wildcard $(SRCDIR)/*.c)
+OBJ      = $(notdir $(SRC:.c=.o))
+
+#========================
 
 all: build install
 
 build:
-	cd src/                             \
-	&& $(CCOMPILER) $(CFLAGS) -c $(SRC) \
-	&& mv $(OBJ) ../$(BUILD)/           \
-	&& cd ../$(BUILD)/                  \
-	&& $(CCOMPILER) $(CFLAGS) -o ../bin/$(TARGET) $(OBJ) $(LFLAGS)
+	if [ ! -d ./$(BINDIR) ]; then mkdir ./$(BINDIR); fi
+	if [ ! -d ./$(BUILDDIR) ]; then mkdir ./$(BUILDDIR); fi
+
+	$(CC) $(CFLAGS) $(LFLAGS) -c $(SRC)
+
+	mv $(OBJ) ./$(BUILDDIR)
+
+	$(CC) $(CFLAGS) -o ./$(BINDIR)/$(TARGET) \
+	$(addprefix ./$(BUILDDIR)/,$(notdir $(OBJ))) $(LFLAGS)
 
 install:
-	cp $(BIN)/$(TARGET) $(PREFIX)/bin/
+	cp ./$(BINDIR)/$(TARGET) $(PREFIX)/
 
 uninstall:
-	rm -vi $(PREFIX)/bin/$(TARGET)
+	rm -vi $(PREFIX)/$(TARGET)
 
 clean:
-	rm -rvI $(BUILD)/*
-	rm -vi $(BIN)/*
+	rm -rv ./$(BINDIR)/*
+	rm -vi ./$(BUILDDIR)/*
 
 .PHONY: all build install uninstall clean
--- src/reduce.c
+++ src/reduce.c
@@ -1,25 +1,37 @@
-/* SPDX-License-Identifier: MIT */
-
 /*
  * Copyright (c) 2022 Roland Gehl
  *
- * reduce.c - Functions for simplification and approximation.
+ * Licensed under the MIT License.
+ * See LICENSE for more details.
  */
 
-#include "rr.h"
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define _POSIX_C_SOURCE 200809L
+
+struct reduced_exp {
+        int place; /* Multiplier */
+        double ans; /* Radicand */
+};
+
+static double root(int degree, int radicand);
+static struct reduced_exp reduce_exp(int degree, int radicand);
 
 /* Return an accurate approximation of a root using Newton's method. */
-double
+static double
 root(int degree, int radicand)
 {
         /* Is there a way to find a more accurate first guess? */
         double x_past = rand() % radicand; /* First guess */
-        double x_current;
+        double x_current = 0;
 
         /* Need a better way to find a good amount of iterations. This is fine
            for now though. */
+        int i = 0;
         int iter = (int)(ceil(20 * log2(degree)));
-        for (int i = 0; i < iter; ++i) {
+        for (; i < iter; i++) {
                 x_current = ((degree - 1) * x_past + (double)radicand
                         / pow(x_past, degree - 1)) / (double)degree;
                 x_past = x_current;
@@ -29,7 +41,7 @@ root(int degree, int radicand)
 }
 
 /* Return a reduced expression for the provided value. */
-struct reduced_exp
+static struct reduced_exp
 reduce_exp(int degree, int radicand)
 {
         struct reduced_exp exp;
@@ -40,7 +52,8 @@ reduce_exp(int degree, int radicand)
         double results[(int)(floor(exp.ans) + 2)];
 
         /* Floating points are hard sometimes, hence int q, for now. */
-        for (int i = 1; i < (floor(root_val) + 1); ++i) {
+        int i = 1;
+        for (; i < (floor(root_val) + 1); i++) {
                 results[i] = pow((root_val / i), degree);
                 int q = (int)(fmod(results[i], 1) * 100);
                 if (q == 0 || q == 99 || q == 100) {
@@ -50,4 +63,44 @@ reduce_exp(int degree, int radicand)
         }
 
         return exp;
+}
+
+int
+main(void)
+{
+        int degree, radicand, imaginary = 0;
+
+        /* scanf() is prone to error, it should be replaced. */
+        printf("Degree: ");
+        scanf("%d", &degree);
+        printf("Radicand: ");
+        scanf("%d", &radicand);
+        printf("\n");
+
+        /* Account for 'lateral' numbers. */
+        if (radicand < 0) {
+                radicand *= -1;
+                imaginary = 1;
+        }
+
+        struct reduced_exp exp = reduce_exp(degree, radicand);
+
+        /*
+         * Replace this with a more modular (and better) way of display.
+         * In addition, decimal points should be truncated.
+         */
+        switch (imaginary) {
+        case 0:
+                if (exp.place <= 1)
+                        printf("√%f\n", exp.ans);
+                else
+                        printf("%d√%f\n", exp.place, exp.ans);
+                break;
+        case 1:
+                if (exp.place <= 1)
+                        printf("i√%f\n", exp.ans);
+                else
+                        printf("%di√%f\n", exp.place, exp.ans);
+                break;
+        }
 }
